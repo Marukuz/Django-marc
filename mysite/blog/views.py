723ffca5bf0e4a37b7a3b652fa7fda9ca47e17b0
Marc Garrido <marccocmc@gmail.com>
2023-02-22 09:07:06 +0100

    commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
@@ -47,4 +47,17 @@ def post_edit(request, pk):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
     
